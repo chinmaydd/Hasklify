add725e5661cfb35c952c5478602c0190a622abf
Kishor Bhat <kishorbhat@gmail.com>
2015-04-08 11:42:09 +0530

    add main, comments

--- heap/heapsort.hs
+++ heap/heapsort.hs
@@ -1,15 +1,25 @@
 -- Haskell program to perform heapsort
 import Data.Graph.Inductive.Internal.Heap(
   Heap(..),insert,findMin,deleteMin
+
 -- Build the given array into heap
 build :: Ord a => [(a,b)] -> Heap a b
 build = foldr insert Empty
+
 -- Max heapify
 toList :: Ord a => Heap a b -> [(a,b)]
 toList Empty = []
 toList h = x:toList r
            where (x,r) = (findMin h,deleteMin h)
--- Begin main
+
+-- Sort the given array
 heapsort :: Ord a => [a] -> [a]
 heapsort = (map fst) . toList . build . map (\x->(x,x))
+
+-- Begin main
+main=do
+	input<-getLine
+	let n=(read input)::[Int]
+	heapsort n
+	return()
 -- End main
