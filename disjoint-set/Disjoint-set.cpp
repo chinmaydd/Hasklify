982b14bf60df274fc7de3b5f2b9aa4021238528c
abgese <abraham.g.sebastian@gmail.com>
2015-04-08 11:39:15 +0530

    Update Disjoint-set.cpp

--- disjoint-set/Disjoint-set.cpp
+++ disjoint-set/Disjoint-set.cpp
@@ -1,14 +1,17 @@
+/*
+C++ implementation of Disjoint-Set Data Structure
+*/
 #include<bits/stdc++.h>
 using namespace std;
-int fnd(int x,int set[])
+int fnd(int x,int set[])   //The find operation of the Data Structure
 {
 	if(set[x]==x)
 		return x;
 	else
-		return fnd(set[x],set);
+		return set[x]=fnd(set[x],set);
 }
 
-void unin(int x,int y,int set[])
+void unin(int x,int y,int set[]) //union operation of Data Structure
 {
 	set[fnd(x,set)]=fnd(y,set);
 }
@@ -18,9 +21,9 @@ int main()
 	int i,j,n,m,x,y;
 	cout<<"Enter the number of elements : ";
 	cin>>n;
-	int set[n];
+	int set[n];     //Disjoint Set
 	for(i=0;i<n;i++)
-		set[i]=i;
+		set[i]=i; //Initializing Disjoint-Set
 	cout<<"Enter the number of operations : ";
 	cin>>m;
 	for(i=0;i<m;i++)
