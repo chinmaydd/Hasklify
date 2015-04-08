ab9c07e022a11e7522cdb76c6544d1082bceaa2b
pvk24 <pvk24@users.noreply.github.com>
2015-04-08 10:50:43 +0530

    Update Stack.hs

--- stack/Stack.hs
+++ stack/Stack.hs
@@ -1,27 +1,27 @@
 
--- This  is an implementation of Stack using Haskell's list operators
+-- This  is an implementation of Stack using Haskell's list operator
 
 -- type synonym for a Stack
 
 type Stack = [Int]
 
-pop :: Stack -> Stack
-pop s = tail s
+pop :: Stack -> Stack --pop function takes stack type as input parameter and returns stack type
+pop s = tail s --'tail' returns all elements except the first element of a list
 
-push :: Int -> Stack -> Stack
-push c s = c:s  
+push :: Int -> Stack -> Stack --push function takes stack type and int type as input parameters and returns stack type
+push c s = c:s  -- 'c:s' adds c to front of list s  
      
-showStack :: Stack -> IO()
+showStack :: Stack -> IO() --showStack function takes stack type as input parameter and prints output on screen(IO)
 showStack [] = error "Stack empty"
 showStack s = print  s
 
 main=do
 	
 	putStrLn "Enter value to be pushed"
-	str<-getLine
-	let n=read str::Int
-	let s=[34,1,23,90]
-	let s1=push n s
+	str<-getLine -- get input
+	let n=read str::Int --typecast input to int type
+	let s=[34,1,23,90] --declaration of list
+	let s1=push n s --lists in haskell are immutable and therefore,a temporary list has been used to make permanent changes
 	let s=s1
 	putStrLn "Stack Status:"
 	showStack s
