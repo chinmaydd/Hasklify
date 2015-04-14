b28e716e8f63ecd9cb2cd95b0259a4aa4b093301
Kishor Bhat <kishorbhat@gmail.com>
2015-04-14 17:05:11 +0530

    Update heapsort.hs

--- heap/heapsort.hs
+++ heap/heapsort.hs
@@ -1,6 +1,6 @@
 -- Haskell program to perform heapsort
 import Data.Graph.Inductive.Internal.Heap(
-  Heap(..),insert,findMin,deleteMin
+  Heap(..),insert,findMin,deleteMin)
 
 -- Build the given array into heap
 build :: Ord a => [(a,b)] -> Heap a b
@@ -18,8 +18,8 @@ heapsort = (map fst) . toList . build . map (\x->(x,x))
 
 -- Begin main
 main=do
-	input<-getLine
-	let n=(read input)::[Int]
-	heapsort n
+	input<-getLine  -- get input
+	let n=(read input)::[Int]  -- convert input to list of integers
+	heapsort n  -- call heapsort function on input list
 	return()
 -- End main
