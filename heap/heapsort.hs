89a90ac998f0b0f65adc177799fc686f437a83f7
Kishor Bhat <kishorbhat@gmail.com>
2015-04-15 08:25:53 +0530

    Update heapsort.hs

--- heap/heapsort.hs
+++ heap/heapsort.hs
@@ -2,12 +2,12 @@
 import Data.Graph.Inductive.Internal.Heap(
   Heap(..),insert,findMin,deleteMin)
 
--- Build the given array into heap
+-- Build the given list into heap
 build :: Ord a => [(a,b)] -> Heap a b  -- build takes a list of (a,b) pairs, returns a heap
 build = foldr insert Empty  -- 
 
 -- Min heapify
-toList :: Ord a => Heap a b -> [(a,b)]
+toList :: Ord a => Heap a b -> [(a,b)]  -- toList takes a heap and returns a list of ordered tuples
 toList Empty = []
 toList h = x:toList r
            where (x,r) = (findMin h,deleteMin h)
@@ -15,7 +15,8 @@ toList h = x:toList r
 -- Sort the given array
 heapsort :: Ord a => [a] -> [a]  -- heapsort takes a list and returns a list
 heapsort = (map fst) . toList . build . map (\x->(x,x))  {- takes the list, creates a pair (x,x) for each element, calls build, 
-converts returned heap to list, and applies fst to each element of the heap -}
+converts returned heap to list, and applies fst to each element of the heap. fst returns a list of the first elements of each 
+tuple in the given list -}
 
 -- Begin main
 main=do
